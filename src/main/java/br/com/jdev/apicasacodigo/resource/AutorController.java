82221111333dbda6c3529ec6e63da2b70ac43d45
= <=>
2020-07-30 22:57:06 -0300

    Versao dev eficiente, init binder

--- src/main/java/br/com/jdev/apicasacodigo/resource/AutorController.java
+++ src/main/java/br/com/jdev/apicasacodigo/resource/AutorController.java
@@ -7,7 +7,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,14 @@ public class AutorController {
 
   @PersistenceContext
   EntityManager entityManager;
+  @Autowired
+  private ProibeEmailDuplicadoValidator proibeEmailDuplicadoAutorValidator;
+
+  @InitBinder
+  public void init(WebDataBinder dataBinder){
+    dataBinder.addValidators(proibeEmailDuplicadoAutorValidator);
+
+  }
 
   @Transactional
   @PostMapping
---
+++ src/main/java/br/com/jdev/apicasacodigo/resource/ProibeEmailDuplicadoValidator.java
@@ -0,0 +1,37 @@
+package br.com.jdev.apicasacodigo.resource;
+
+import br.com.jdev.apicasacodigo.dto.AutorRequest;
+import br.com.jdev.apicasacodigo.model.Autor;
+import br.com.jdev.apicasacodigo.repository.AutorRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class ProibeEmailDuplicadoValidator implements Validator {
+
+  @Autowired
+  private AutorRepository autorRepository;
+
+  @Override
+  public boolean supports(Class<?> aClass) {
+    return AutorRequest.class.isAssignableFrom(aClass);
+  }
+
+  @Override
+  public void validate(Object object, Errors errors) {
+    if (errors.hasErrors()) {
+      return;
+    }
+    AutorRequest autorRequest = (AutorRequest) object;
+
+    Optional<Autor> autorOpt = autorRepository.findByEmail(autorRequest.getEmail());
+
+    autorOpt.ifPresent((autor) -> errors.rejectValue("email", null,
+        String.format("Já existe autor com o email informado %s", autor.getEmail())));
+
+
+  }
+}
