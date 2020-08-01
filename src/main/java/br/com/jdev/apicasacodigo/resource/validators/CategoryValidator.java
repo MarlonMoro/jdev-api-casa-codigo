c779a1a849446004c38d43116308834ed9f7d85a
= <=>
2020-08-01 13:49:29 -0300

    Corrigido rejectValue no validator

--- src/main/java/br/com/jdev/apicasacodigo/resource/validators/CategoryValidator.java
+++ src/main/java/br/com/jdev/apicasacodigo/resource/validators/CategoryValidator.java
@@ -1,7 +1,9 @@
 package br.com.jdev.apicasacodigo.resource.validators;
 
 import br.com.jdev.apicasacodigo.dto.CategoryRequest;
+import br.com.jdev.apicasacodigo.model.Category;
 import br.com.jdev.apicasacodigo.repository.CategoryRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -26,7 +28,7 @@ public class CategoryValidator implements Validator {
     CategoryRequest categoryRequest = (CategoryRequest) o;
 
     categoryRepository.findByName(categoryRequest.getName()).ifPresent((category) -> {
-      errors.rejectValue("Categoria já cadastrada", null,
+      errors.rejectValue("name", null,
           String.format("Categoria %s já cadastrada", categoryRequest.getName()));
     });
 
