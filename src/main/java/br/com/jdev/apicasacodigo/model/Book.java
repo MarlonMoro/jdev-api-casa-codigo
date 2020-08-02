dc52022cd5eaec77a4f29d00597df6a490d68392
= <=>
2020-08-02 13:25:41 -0300

    Book initial commit

---
+++ src/main/java/br/com/jdev/apicasacodigo/model/Book.java
@@ -0,0 +1,15 @@
+package br.com.jdev.apicasacodigo.model;
+
+import java.math.BigDecimal;
+
+public class Book {
+
+  private Long id;
+  private String title;
+  private String resume;
+  private String summary;
+  private BigDecimal price;
+  private Integer pageNumber;
+
+
+}
